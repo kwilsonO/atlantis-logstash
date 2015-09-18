f0c1092ca597751b0d98faeb391bac4c50a69b5f
root <root@ip-10-144-206-29.ec2.internal>
2015-09-18 19:37:37 +0000

    more updates to java8 install for automation

--- scripts/setup/install-java8.sh
+++ scripts/setup/install-java8.sh
@@ -1,5 +1,7 @@
 echo "Installing java 8 for logstash..."
-sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get --assume-yes install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get --assume-yes update
-sudo apt-get --assume-yes install oracle-java8-installer
-sudo apt-get --assume-yes install oracle-java8-set-default
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get --assume-yes install -y oracle-java8-installer
+sudo apt-get --assume-yes install -y oracle-java8-set-default
