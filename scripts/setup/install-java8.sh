c48761fb8f97d4466d89440d224bf13483d4f483
root <root@ip-10-144-206-29.ec2.internal>
2015-09-18 19:31:55 +0000

    updated setup scripts to assume yes on prompts to automate

--- scripts/setup/install-java8.sh
+++ scripts/setup/install-java8.sh
@@ -1,5 +1,5 @@
 echo "Installing java 8 for logstash..."
 sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
+sudo apt-get --assume-yes update
+sudo apt-get --assume-yes install oracle-java8-installer
+sudo apt-get --assume-yes install oracle-java8-set-default
