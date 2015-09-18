d78e2e17fca6ebd0b957d7f574b5040519498bf4
root <root@ip-10-144-206-29.ec2.internal>
2015-09-18 19:47:56 +0000

    fixed bash sytax error

--- scripts/setup/logstash-setup.sh
+++ scripts/setup/logstash-setup.sh
@@ -5,6 +5,6 @@ wget "${LS_DL_URL}"
 tar -xzf "logstash-${LS_VERSION}.tar.gz"
 rm "logstash-${LS_VERSION}.tar.gz"
 
-if [ ! -d "${LS_LOG_PATH" ]; then 
+if [ ! -d "${LS_LOG_PATH}" ]; then 
 	mkdir -p ${LS_LOG_PATH}
 fi
